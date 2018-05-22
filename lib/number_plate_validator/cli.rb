204a71db16725dda0edd51d169c766eeb966e28d
Edgar <zorro.ej@gmail.com>
2018-05-22 12:09:56 +0800

    pass feature

--- lib/number_plate_validator/cli.rb
+++ lib/number_plate_validator/cli.rb
@@ -1,12 +1,13 @@
 require 'thor'
+require 'number_plate_validator'
 
 module NumberPlateValidator
 	class CLI < Thor
 		desc "validate -c CC PLATE", "validate if a PLATE is valid in the country CC"
 		method_option :country, aliases: "-c", desc: "standard country code in capital letters", required: true
 		def validate(plate)
-      validator = NumberPlateValidator.validator(options[:country])
-			validator.is_valid? plate
+      validator = NumberPlateValidator::Validator.new(options[:country])
+			puts(validator.is_valid? plate)
 		end
 
 	end
