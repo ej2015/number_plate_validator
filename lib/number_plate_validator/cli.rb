15bf878409ba68a5b8f5848ab972f48177a9528b
Edgar <zorro.ej@gmail.com>
2018-05-29 09:14:47 +0800

    adjust cli desc

--- lib/number_plate_validator/cli.rb
+++ lib/number_plate_validator/cli.rb
@@ -3,7 +3,7 @@ require 'number_plate_validator'
 
 module NumberPlateValidator
 	class CLI < Thor
-		desc "validate -c CC PLATE", "validate if a PLATE is valid in the country CC"
+		desc "validate -c COUNTRY PLATE", "validate if a PLATE is valid in the country CC"
 		method_option :country, aliases: "-c", desc: "standard country code in capital letters", required: true
 		def validate(plate)
       validator = NumberPlateValidator.validator(options[:country])
